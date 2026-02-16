decfe038a9fc26781510fdcb8aa48d722caa73d2
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-02-16 16:47:13 +0400

    Added a Swiper-based news slider component with navigation arrows and loop support. Integrated dynamic image and content rendering from a structured data array using Next.js Image for optimized loading. Styled the layout to position overlay text and enable left/right navigation similar to the reference design.

--- embark-studio-clone/app/components/Hero.jsx
+++ embark-studio-clone/app/components/Hero.jsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 
 const Hero = () => {
+  const newsCardItems = [
+    {
+      image: "@/WEB_News_Banne_ARC_TT2.jpg",
+      label: "ARC Raiders | Thank you Raiders!",
+      description:
+        "ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
+    },
+  ];
   return (
     <section className="hero">
       <div className="heroMain">
@@ -18,6 +26,24 @@ const Hero = () => {
             </Link>
           </div>
         </div>
+        <section className="newsSection">
+          <div className="container">
+            <div className="sectionTitle">
+              <h2>Latest News</h2>
+            </div>
+            <div className="newsSectionCards">
+              {newsCardItems.map((item, index) => (
+                <div key={index} className="newsSectionCard">
+                  <div className="newsSectionCardImage">
+                    <img src={item.image} alt={item.label} />
+                  </div>
+                  <div className="newsSectionTitle">{item.label}</div>
+                  <div className="newsCardDesription">{item.description}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   );
