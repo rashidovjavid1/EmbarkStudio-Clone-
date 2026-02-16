9b4689c28d6cb3dd0494b702909392644cf3a0c8
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-02-16 21:19:50 +0400

    Enhanced the news section with structural improvements and additional layout refinements. Applied CSS adjustments to improve alignment, spacing, and visual consistency. Optimized styling for cleaner and more stable presentation.

--- embark-studio-clone/app/components/Hero.jsx
+++ embark-studio-clone/app/components/Hero.jsx
@@ -7,6 +7,30 @@ const Hero = () => {
       label: "ARC Raiders | Thank you Raiders!",
       description:
         "ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
+      tag: "enlist.resist",
+    },
+    {
+      image: "/WEB_News_Banner_Deluxe.jpg",
+      label: "ARC RAIDERS | Pre-Order Now!",
+      description:
+        "The Rust Belt awaits! Pre-Orders are available for Arc Raiders now! Get the Deluxe Edition to earn the exclusive Astro Bundle. Haven't played yet? Don't miss our Server Slam open test happening Oct 17-19! It's free to join,so invite your friends and get ready to head Topside,Raiders!",
+      tag: "ARC Raiders will launch on October 30",
+    },
+
+    {
+      image: "/WEB_News_Banner_s3_emerald.jpg",
+      label: "THE FINALS | World Tour Emerald",
+      description:
+        "It ain't easy being green.. but it worth a try!Win tournaments and unlock every Emerald reward in Season 3! Play THE FINALS World Tour now!",
+      tag: "",
+    },
+
+    {
+      image: "/WEB_News_Banner_1.6.0.jpg",
+      label: "THE FINALS | Steal The Spotlight Event",
+      description:
+        "In this limited Mid-season. Event:the lights are out and cashout stations are guarded by turrets and tripwires in the special Solo Bank it mode!",
+      tag: "",
     },
   ];
   return (
@@ -43,6 +67,9 @@ const Hero = () => {
                     </a>
                     <a href="#">
                       <p>{item.description}</p>
+                      <div className="tag">
+                        <span>{item.tag}</span>
+                      </div>
                     </a>
                   </div>
                 </div>
