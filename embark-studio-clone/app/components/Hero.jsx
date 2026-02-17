11e5dd14a379fa4f95d5d273bd4b26815e4c64b0
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-02-18 01:48:22 +0400

    Added a structured footer section including navigation links and layout styling, integrated it into the main page hierarchy, and aligned spacing/typography with the existing design system.

--- embark-studio-clone/app/components/Hero.jsx
+++ embark-studio-clone/app/components/Hero.jsx
@@ -7,27 +7,28 @@ const Hero = () => {
       image: "/WEB_News_Banne_ARC_TT2.jpg",
       label: "ARC Raiders | Thank you Raiders!",
       description:
-        "ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
+        "ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
       tag: "enlist.resist",
     },
     {
       image: "/WEB_News_Banner_Deluxe.jpg",
       label: "ARC RAIDERS | Pre-Order Now!",
       description:
-        "The Rust Belt awaits! Pre-Orders are available for Arc Raiders now!",
-      tag: "ARC Raiders will launch on October 30",
+        "The Rust Belt awaits! Pre-Orders are available for Arc Raiders now! ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
+      tag: "ARC Raiders will launch on October 30 ",
     },
     {
       image: "/WEB_News_Banner_s3_emerald.jpg",
       label: "THE FINALS | World Tour Emerald",
       description:
-        "Win tournaments and unlock every Emerald reward in Season 3!",
+        "Win tournaments and unlock every Emerald reward in Season 3! ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
       tag: "",
     },
     {
       image: "/WEB_News_Banner_1.6.0.jpg",
       label: "THE FINALS | Steal The Spotlight Event",
-      description: "In this limited Mid-season Event the lights are out.",
+      description:
+        "In this limited Mid-season Event the lights are out. ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long",
       tag: "",
     },
 
@@ -35,8 +36,22 @@ const Hero = () => {
       image: "/makingTHEFINALS.jpg",
       label: "Creating a Dynamism Shooter | THE FINALS",
       description:
-        "When we started making THE FINALS, we wanted to bring together all the things we always loved in games: Dynamism! In thi episode of MAKING THE FINALS, our Creative Director, Gustav Tilleby Silow, explains Dynamism and walks us through the journey of making the world's first Dynamism Shooter.",
+        "When we started making THE FINALS, we wanted to bring together all the things we always loved in games: Dynamism! In thi episode of MAKING THE FINALS, our Creative Director, Gustav Tilleby Silow, explains Dynamism and walks us through the journey of making the world's first Dynamism Shooter. ARC activity on the surface has receded for now.So you can take it easy for a while,get some rest,stretch your legs,maybe take scrappy for a walk?Just don't wonder too far. The Rust Belt never stays quiet for long ",
     },
+
+    {
+      image: "/WEB_News_Banner_s8.jpg",
+      label: "THE FINALS | GAME TIME",
+      description:
+        "Season 8 is here!This is the main event,Contestants with all eyes on the grandest of stages, if there ever was a moment to play your best, this is it.The game is on.The greatest gameshow on Earth is back for another season!This is THE FINALS",
+    },
+  ];
+  const linkItems = [
+    "Embark HQ",
+    "Contact Us",
+    "Privacy policy",
+    "Security",
+    "Cookie Settings",
   ];
 
   return (
@@ -65,6 +80,25 @@ const Hero = () => {
 
             <div className="newsSectionCards">
               <HeroSlider items={newsCardItems} />
+            </div>
+          </div>
+        </section>
+        <section className="bottomSection">
+          <div className="container">
+            <div className="linksSection">
+              {linkItems.map((item, index) => (
+                <a key={index} href="#">
+                  <span>{item}</span>
+                </a>
+              ))}
+            </div>
+            <div className="privacyPolicy">
+              <p>
+                © 2026 Embark Studios AB. ARC RAIDERS, THE FINALS and EMBARK
+                trademarks and logos are trademarks or registered trademarks of
+                Embark Studios AB. All other trademarks and trade names are the
+                property of their respective owners.
+              </p>
             </div>
           </div>
         </section>
